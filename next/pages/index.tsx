261b0a40769e53a6479ab472ec4c8f9ad2850540
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-03-25 23:27:31 +0100

    styling added

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -11,15 +11,70 @@ export default function Home() {
 				<title>Next</title>
 			</Head>
 			<main>
-				<h1>Next</h1>
-				<h2>Hello World</h2>
-				<HelloWorld />
-				<h2>Counter</h2>
-				<Counter />
-				<h2>Temperature Converter</h2>
-				<TemperatureConverter />
-				<h2>Flight Booker</h2>
-				<FlightBooker />
+				<h1>AS GUIs Next</h1>
+
+				<p>
+					This is a live version of an implementation of{' '}
+					<a href="https://eugenkiss.github.io/7guis/">7GUIs</a> (+ extra) in{' '}
+					<a href="https://nextjs.org/">Next</a> (<a href="https://reactjs.org/">React</a>).
+				</p>
+				<ul>
+					<li>
+						<a href="https://github.com/SarcevicAntonio/as-guis/">🐙 Visit project repo</a>
+					</li>
+					<li>
+						<a href="https://github.com/SarcevicAntonio/as-guis/tree/main/next/lib">
+							📝 Browse component source code.
+						</a>
+					</li>
+				</ul>
+
+				<div className="articles">
+					<article>
+						<div className="title">
+							<h2>Hello World</h2>
+							<a href="https://github.com/SarcevicAntonio/as-guis/blob/main/next/lib/HelloWorld.tsx">
+								📝
+							</a>
+						</div>
+						<div className="content">
+							<HelloWorld />
+						</div>
+					</article>
+					<article>
+						<div className="title">
+							<h2>Counter</h2>
+							<a href="https://github.com/SarcevicAntonio/as-guis/blob/main/next/lib/Counter.tsx">
+								📝
+							</a>
+						</div>
+						<div className="content">
+							<Counter />
+						</div>
+					</article>
+					<article>
+						<div className="title">
+							<h2>Temperature Converter</h2>
+							<a href="https://github.com/SarcevicAntonio/as-guis/blob/main/next/lib/TemperatureConverter.tsx">
+								📝
+							</a>
+						</div>
+						<div className="content">
+							<TemperatureConverter />
+						</div>
+					</article>
+					<article>
+						<div className="title">
+							<h2>Flight Booker</h2>
+							<a href="https://github.com/SarcevicAntonio/as-guis/blob/main/next/lib/FlightBooker.tsx">
+								📝
+							</a>
+						</div>
+						<div className="content">
+							<FlightBooker />
+						</div>
+					</article>
+				</div>
 			</main>
 		</>
 	);
--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -5,15 +5,78 @@ useMeta({
 </script>
 <template>
 	<main>
-		<h1>Nuxt</h1>
-		<h2>Hello World</h2>
-		<HelloWorld />
-		<h2>Counter</h2>
-		<Counter />
-		<h2>Temperature Converter</h2>
-		<TemperatureConverter />
-		<h2>Flight Booker</h2>
-		<FlightBooker />
+		<h1>AS GUIs Nuxt</h1>
+
+		<p>
+			This is a live version of an implementation of
+			<a href="https://eugenkiss.github.io/7guis/">7GUIs</a> (+ extra) in
+			<a href="https://nuxtjs.org/">Nuxt</a> (<a href="https://vuejs.org/">Vue</a>).
+		</p>
+		<ul>
+			<li>
+				<a href="https://github.com/SarcevicAntonio/as-guis/">🐙 Visit project repo</a>
+			</li>
+			<li>
+				<a href="https://github.com/SarcevicAntonio/as-guis/tree/main/nuxt/components">
+					📝 Browse component source code.
+				</a>
+			</li>
+		</ul>
+
+		<div class="articles">
+			<article>
+				<div class="title">
+					<h2>Hello World</h2>
+					<a
+						href="https://github.com/SarcevicAntonio/as-guis/blob/main/nuxt/components/HelloWorld.vue"
+					>
+						📝
+					</a>
+				</div>
+				<div class="content">
+					<HelloWorld />
+				</div>
+			</article>
+			<article>
+				<div class="title">
+					<h2>Counter</h2>
+					<a
+						href="https://github.com/SarcevicAntonio/as-guis/blob/main/nuxt/components/Counter.vue"
+					>
+						📝
+					</a>
+				</div>
+				<div class="content">
+					<Counter />
+				</div>
+			</article>
+			<article>
+				<div class="title">
+					<h2>Temperature Converter</h2>
+					<a
+						href="https://github.com/SarcevicAntonio/as-guis/blob/main/nuxt/components/TemperatureConverter.vue"
+					>
+						📝
+					</a>
+				</div>
+				<div class="content">
+					<TemperatureConverter />
+				</div>
+			</article>
+			<article>
+				<div class="title">
+					<h2>Flight Booker</h2>
+					<a
+						href="https://github.com/SarcevicAntonio/as-guis/blob/main/nuxt/components/FlightBooker.vue"
+					>
+						📝
+					</a>
+				</div>
+				<div class="content">
+					<FlightBooker />
+				</div>
+			</article>
+		</div>
 	</main>
 </template>
 3
