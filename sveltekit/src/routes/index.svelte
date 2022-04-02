d1e48f82f050083f01b33d70486bd1dd5e63ae9d
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-04-03 00:43:37 +0200

    added styles per hand

--- sveltekit/src/routes/index.svelte
+++ sveltekit/src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../../../styles.css';
+	import '../styles.css';
 	import Counter from '$lib/Counter.svelte';
 	import FlightBooker from '$lib/FlightBooker.svelte';
 	import HelloWorld from '$lib/HelloWorld.svelte';
