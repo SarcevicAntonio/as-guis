71ebe725b240928a212ec94addc56667df24d7f7
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-02 15:48:09 +0200

    add svelte

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), react(), vue()]
+});
