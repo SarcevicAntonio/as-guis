3d383ab32cea4d6d8ac25a4485e56b0e692dabc3
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-02 17:23:31 +0200

    swag 1

--- astro/src/components/HelloWorld/index.js
+++ astro/src/components/HelloWorld/index.js
@@ -1,9 +1,17 @@
 import SvelteDemo from "./HelloWorld.svelte";
 import SvelteCode from "./HelloWorld.svelte?raw";
+import VueDemo from "./HelloWorld.vue";
+import VueCode from "./HelloWorld.vue?raw";
+
+
 
 export default {
   svelte: {
     demo: SvelteDemo,
     code: SvelteCode,
+  },
+  vue: {
+    demo: VueDemo,
+    code: VueCode,
   },
 };
